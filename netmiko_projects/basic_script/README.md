448089bc1fc8d19fd64c6dcec84c4db02e4280c2
root <root@centos8.fritz.box>
2020-06-24 21:27:38 +0200

    Added basic netmiko script

---
+++ netmiko_projects/basic_script/README.md
@@ -0,0 +1,6 @@
+Creating the basic netmiko script. 
+Only one command is pushed to destination device.
+
+Additionaly in order to save the output command, we specified below command.
+
+$python3 basic_script.py >>  bigip_config.cfg
---
+++ netmiko_projects/basic_script/basic_script.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+
+iosv_l2_s1 = {
+    'device_type': 'f5_bigip',
+    'ip': '192.168.178.23',
+    'username': 'root',
+    'password': '!mlh1985',
+}
+
+net_connect = ConnectHandler(**iosv_l2_s1)
+output = net_connect.send_command('cat /config/bigip.conf')
+print(output)
+
+
