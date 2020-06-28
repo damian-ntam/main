6a82fd1e430b5b3ee8f5a7d36822498c65bad446
melih <melihteke@gmail.com>
2020-06-28 12:10:12 +0200

    added some changes

--- netmiko_projects/basic_script/basic_script.py
+++ netmiko_projects/basic_script/basic_script.py
@@ -1,14 +1,13 @@
+
 from netmiko import ConnectHandler
 
 iosv_l2_s1 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.178.23',
-    'username': 'cisco',
-    'password': 'admin',
+    'ip': '192.168.178.65',
+    'username': 'admin',
+    'password': 'cisco',
 }
 
 net_connect = ConnectHandler(**iosv_l2_s1)
-output = net_connect.send_command('show version')
+output = net_connect.send_command('show flash:')
 print(output)
-
-
